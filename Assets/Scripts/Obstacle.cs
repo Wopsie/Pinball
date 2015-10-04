972f3177de6bd658d1879330d34b0774c81d558d
Wopsie <joaghim.beltman@gmail.com>
2015-10-04 13:30:38 +0200

    added Obstacle scaling upon collision with ball

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour 
+{
+
+	void FixedUpdate () 
+    {
+        
+	}
+
+    void OnCollisionStay(Collision col)
+    {
+        if(col.gameObject.tag == "Ball")
+        {
+            ScaleBumperUp();
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if(col.gameObject.tag == "Ball")
+        {
+            ScaleBumperDown();
+        }
+    }
+
+    void ScaleBumperUp()
+    {
+        if(transform.localScale.x <= 6.5f && transform.localScale.z <= 6.5f)
+        {
+            transform.localScale += new Vector3(0.5f, 0, 0.5f)*(Time.deltaTime*80);
+        }
+    }
+
+    void ScaleBumperDown()
+    {
+        transform.localScale = new Vector3(5f, 1f, 5f);
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour 
+{
+    [SerializeField]
+    private Text scoreText;
+    private int score;
+
+
+	void Update () 
+    {
+        scoreText.text = "Score: " + score;
+        score++;
+        //if collision with 
+	}
+}
