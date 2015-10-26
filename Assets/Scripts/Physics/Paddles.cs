d906fa29e68f2d60a478f038268ac83300eccdb2
Wopsie <joaghim.beltman@gmail.com>
2015-10-26 09:03:04 +0100

    alles binnenhalen

--- Assets/Scripts/Physics/Paddles.cs
+++ Assets/Scripts/Physics/Paddles.cs
@@ -9,21 +9,65 @@ public class Paddles : MonoBehaviour {
     private GameObject leftFlipper;
     private GameObject rightFlipper;
 
-
     void Awake()
     {
-        leftFlipper = GameObject.Find("LeftFlipper");
+       leftFlipper = GameObject.Find("LeftFlipper");
+       rightFlipper = GameObject.Find("RightFlipper");
     }
-	// Use this for initialization
-	void Start () {
+	
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+    }
+
+	void Start () 
+    {
+        if (gameObject.name == "LeftFlipper")
+        {
+            leftTrigger = true;
+        }
+
+        if (gameObject.name == "RightFlipper")
+        {
+            rightTrigger = true;
+        }
+
+        leftFlipper.GetComponent<Rigidbody>().centerOfMass = new Vector3(-0.1f, 0);
+        rightFlipper.GetComponent<Rigidbody>().centerOfMass = new Vector3(0.1f, 0);
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
+	void FixedUpdate () {
+        Debug.DrawRay(this.GetComponent<Rigidbody>().worldCenterOfMass, Vector3.up, Color.red);
+
+        if (leftTrigger)
         {
-            Debug.Log(leftFlipper.activeSelf);
-            //leftFlipper.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward);
+            if (Input.GetKey(KeyCode.A))
+            {
+                this.GetComponent<Rigidbody>().AddForce(Vector3.back * 1000, ForceMode.Impulse);
+            }
+            else
+            {
+                this.GetComponent<Rigidbody>().AddForce(Vector3.forward * 2000);
+            }
         }
-	}
+
+        if (rightTrigger)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.GetComponent<Rigidbody>().AddForce(Vector3.back * 1000, ForceMode.Impulse);
+            }
+            else
+            {
+                this.GetComponent<Rigidbody>().AddForce(Vector3.forward * 2000);
+            }
+        }
+        
+            
+            
+        
+           
+        }
+	
 }
