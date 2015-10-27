7d1f4d0aab35e030e10e82ae21ef1df837a224bc
Wopsie <joaghim.beltman@gmail.com>
2015-10-27 11:42:38 +0100

    fixed black pointbars collision

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -12,7 +12,7 @@ public class ScoreManager : MonoBehaviour
     {
         scoreText.text = "Score: " + score;
 	}
-
+    /*
     void OnCollisionExit(Collision col)
     {
         switch(col.gameObject.tag)
@@ -22,12 +22,40 @@ public class ScoreManager : MonoBehaviour
                 break;
             case "FarLine":
                 score += 100;
+                Debug.Log("touched line");
                 break;
             case "MediumLine":
                 score += 50;
+                Debug.Log("touched line");
                 break;
             case "CloseLine":
                 score += 30;
+                Debug.Log("touched line");
+                break;
+            case "DoorSwitch":
+                score += 100;
+                break;
+        }
+    }*/
+
+    void OnTriggerEnter(Collider col)
+    {
+        switch (col.gameObject.tag)
+        {
+            case "Bumper":
+                score += 25;
+                break;
+            case "FarLine":
+                score += 100;
+                Debug.Log("touched line");
+                break;
+            case "MediumLine":
+                score += 50;
+                Debug.Log("touched line");
+                break;
+            case "CloseLine":
+                score += 30;
+                Debug.Log("touched line");
                 break;
             case "DoorSwitch":
                 score += 100;
---
+++ Assets/Scripts/VelocityManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VelocityManager : MonoBehaviour 
+{
+    private bool bumperColl;
+    private Vector3 velocity;
+    private int velocityTimer;
+
+	void Start () 
+    {
+        velocityTimer = 0;
+	}
+	
+	void Update ()
+    {
+        if(velocityTimer > 0)
+        {
+            velocityTimer++;
+            VelocityIncrease();
+            if(velocityTimer > 50)
+            {
+                velocityTimer = 0;
+            }
+        }
+        else
+        {
+            velocityTimer = 0;
+        }
+
+        //Debug.Log("velocity timer = "+velocityTimer);
+	}
+
+    void OnCollisionExit(Collision col)
+    {
+        if(col.gameObject.tag == "Bumper")
+        {
+            velocityTimer++;
+        }
+    }
+
+    void VelocityIncrease()
+    {
+        transform.Translate(velocity * Time.deltaTime * 50);
+    }
+}
