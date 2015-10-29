1178ccb0d635f78c2dfb464476634282f94b6c6a
Wopsie <joaghim.beltman@gmail.com>
2015-10-29 15:23:11 +0100

    added sceneswitch script & leaderboard scene

--- Assets/Scripts/Highscore/PostURL.cs
+++ Assets/Scripts/Highscore/PostURL.cs
@@ -6,24 +6,20 @@ public class PostURL : MonoBehaviour
 {
     [SerializeField]
     private Text scoreBoardText;
-    public string url = "http://localhost/pinballscores/";
+    public string url = "19176.hosts.ma-cloud.nl/bewijzenmap/jaar2/PRO/Pinball/";
     private string _player = "";
     private string _score = "";
     private string urlString;
 
-    
-
-    
-
     void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 50, 25), "Name");
-        _player = GUI.TextField(new Rect(70, 10, 100, 25), _player);
-        GUI.Label(new Rect(360, 10, 50, 25), "Score");
-        _score = GUI.TextField(new Rect(400, 10, 100, 25), _score);
+        //creates textfield for playername and converts score int to string.
+        GUI.Label(new Rect(410, 480, 50, 25), "Name");
+        _player = GUI.TextField(new Rect(470, 480, 100, 25), _player);
+        _score = ScoreManager.score.ToString();
 
         //save playername
-        if (GUI.Button(new Rect(180, 10, 80, 25), "Send Name"))
+        if (GUI.Button(new Rect(580, 480, 80, 25), "Submit"))
         {
             StartCoroutine("SaveName");
         }
@@ -40,33 +36,4 @@ public class PostURL : MonoBehaviour
         Debug.Log(postName.text);
         scoreBoardText.text = postName.text;
     }
-    
-    /*
-    void Start()
-    {
-        WWWForm form = new WWWForm();
-        WWW www = new WWW(url, form);
-        StartCoroutine(WaitForRequest(www));
-    }
-
-    
-    IEnumerator WaitForRequest(WWW www)
-    {
-        yield return www;
-        www.text.Split('\n');
-        //Debug.Log(www.text);
-        scoreBoardText.text = www.text;
-        if (www.error == null)
-        {
-            Debug.Log("WWW OK!: " + www.text);
-        }
-        else
-        {
-            Debug.Log("WWW ERROR!: " + www.error);
-        }
-
-    }*/
 }
-
-
-   
